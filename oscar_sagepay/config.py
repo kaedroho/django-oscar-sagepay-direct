4ba467dcb39f6a132431efd4b87c2ebedbfe3c0a
Karl Hobley <karlhobley10@gmail.com>
2014-12-04 12:01:00 +0000

    Fixed test mode urls

--- oscar_sagepay/config.py
+++ oscar_sagepay/config.py
@@ -6,10 +6,10 @@ VENDOR = settings.OSCAR_SAGEPAY_VENDOR
 
 TEST_MODE = getattr(settings, 'OSCAR_SAGEPAY_TEST_MODE', True)
 if TEST_MODE:
-    VPS_REGISTER_URL = 'https://test.sagepay.com/Simulator/VSPDirectGateway.asp'
-    VPS_AUTHORISE_URL = 'https://test.sagepay.com/Simulator/VSPServerGateway.asp?Service=VendorAuthoriseTx'
-    VPS_REFUND_URL = 'https://test.sagepay.com/Simulator/VSPServerGateway.asp?Service=VendorRefundTx'
-    VPS_VOID_URL = 'https://test.sagepay.com/Simulator/VSPServerGateway.asp?Service=VendorVoidTx'
+    VPS_REGISTER_URL = 'https://test.sagepay.com/gateway/service/vspdirect-register.vsp'
+    VPS_AUTHORISE_URL = 'https://test.sagepay.com/gateway/service/authorise.vsp'
+    VPS_REFUND_URL = 'https://test.sagepay.com/gateway/service/refund.vsp'
+    VPS_VOID_URL = 'https://test.sagepay.com/gateway/service/void.vsp'
 else:
     VPS_REGISTER_URL = 'https://live.sagepay.com/gateway/service/vspdirect-register.vsp'
     VPS_AUTHORISE_URL = 'https://live.sagepay.com/gateway/service/authorise.vsp'
